ad3a1883a0e8c6cc0794620d710182645a6bd772
AugustoFerraria <augustoferrarialuppino@gmail.com>
2024-06-06 15:51:35 +0200

    Create the ProfilePage

---
+++ src/screens/SplashScreen.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react';
+import { View, ActivityIndicator, StyleSheet } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const SplashScreen = ({ navigation }) => {
+  useEffect(() => {
+    const checkAuth = async () => {
+      const token = await AsyncStorage.getItem('token');
+      if (token) {
+        navigation.navigate('Home');
+      } else {
+        navigation.navigate('Login');
+      }
+    };
+
+    checkAuth();
+  }, [navigation]);
+
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator size="large" color="#0000ff" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default SplashScreen;
